fecbb4295014b1e692077566f9839259a1944e46
Md Juyel Haque <78420794+MdJuyelHaque@users.noreply.github.com>
2023-05-11 03:17:14 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import json
+import streamlit as st
+import time
+from snowflake.snowpark import Session
+
+import pandas as pd
+import json
+import streamlit as st
+from snowflake.snowpark import Session
+import time
+
+if 'snowflake_connection' not in st.session_state:
+    # connect to Snowflake
+    with open('creds.json') as f:
+        connection_parameters = json.load(f)
+    st.session_state.snowflake_connection = Session.builder.configs(connection_parameters).create()
+    session = st.session_state.snowflake_connection
+else:
+    session = st.session_state.snowflake_connection
+
+st.set_page_config(layout="centered", page_title="Data Editor", page_icon="🧮")
+st.title("Demo APP")
+st.caption("This is a demo powered by Thirdeyedata.")
+
+def get_dataset():
+    # load messages df
+    df = session.table("TEDEMO")
+
+    return df
+
+dataset = get_dataset()
+
+with st.form("data_editor_form"):
+     st.caption("Edit the dataframe below")
+     edited = st.experimental_data_editor(dataset, use_container_width=True, num_rows="dynamic")
+     submit_button = st.form_submit_button("Submit")
+
+if submit_button:
+    try:
+        session.write_pandas(edited, "TEDEMO", overwrite=True)
+        st.success("Table updated")
+        time.sleep(5)
+    except:
+        st.warning("Error updating table")
+    #display success message for 5 seconds and update the table to reflect what is in Snowflake
+    st.experimental_rerun()
